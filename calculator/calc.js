061063d5c290b76f551132656a5bfd420ca2be69
Marcin <01169256@pw.edu.pl>
2024-04-24 17:32:20 +0200

    implemented simple function for converting string expression into an array

--- calculator/calc.js
+++ calculator/calc.js
@@ -13,3 +13,35 @@ function displayText(buttonText) {
     else
         calcDisplay.value += buttonText;
 }
+
+function toInfixConvention(string) {
+    // 3 + (123+44)*5 - (222-4)
+    function isDigit(char) {
+        return (char >= '0' && char <= '9');
+    }
+
+    let expresion = [];
+    let number;
+    let i = 0, j;
+    while(i < string.length) {
+        number = "";
+        if(!isDigit(string.charAt(i)) && (i == 0 || !isDigit(string.charAt(i - 1)))) {
+            if (string.charAt(i) !== '-') throw new Error("Incorrect input format");
+            number += string.charAt(i);
+            ++i;
+        }
+        for(j = i; isDigit(string.charAt(j)) && j < string.length; ++j)
+            number += string.charAt(j);
+        
+        if(number != "") {
+            expresion.push(number);
+            i = j;
+        }
+        else {
+            expresion.push(string.charAt(i));
+            ++i;
+        }
+    }
+
+    return expresion;
+}
